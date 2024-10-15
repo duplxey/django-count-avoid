022f03a48dcece8f4d42e6e9610289b38d7c1176
Nik Tomazic <nick.tomazic@gmail.com>
2024-10-16 00:38:11 +0200

    added CountlessPage, CountlessPaginator

--- logs/views.py
+++ logs/views.py
@@ -1,19 +1,20 @@
-from django.core.paginator import Paginator
 from django.http import JsonResponse
 
 from logs.models import Log
+from logs.paginators import CountlessPaginator
 
 
 def index_view(request):
     logs = Log.objects.all()
-    paginator = Paginator(logs, 25)
+
     page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    paginator = CountlessPaginator(logs, 25)
+    page = paginator.get_page(page_number)
 
     return JsonResponse(
         {
-            "has_next": page_obj.has_next(),
-            "has_previous": page_obj.has_previous(),
-            "results": [log.to_json() for log in page_obj],
+            "has_next": page.has_next(),
+            "has_previous": page.has_previous(),
+            "results": [log.to_json() for log in page],
         }
     )
